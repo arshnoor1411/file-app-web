4d66f9abbe8eff404e1f1521ea00333f88c05dbe
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2023-12-12 19:06:35 +0530

    enhance: codegen-file

---
+++ codegen.ts
@@ -0,0 +1,28 @@
+import { envConfig } from "./src/utils/envConfig";
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  generates: {
+    "./src/services/api/fileServer.ts": {
+      schema: envConfig.SERVER_URL + "/graphql",
+      documents: "./src/graphql/user/*.gql",
+      plugins: [
+        {
+          add: {
+            content: "//@ts-nocheck",
+          },
+        },
+        "typescript",
+        "typescript-operations",
+        "typescript-react-query",
+      ],
+      config: {
+        fetcher: "graphql-request",
+        exposeQueryKeys: true,
+        exposeFetcher: true,
+      },
+    },
+  },
+};
+
+export default config;
