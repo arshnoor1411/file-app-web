159a8490eb3e1cb2652894e966b4abd6b38e0cf9
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2023-12-14 11:40:42 +0530

    enhance: added signup mutation

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,22 +1,41 @@
 import SignupForm from "@/components/SignUp";
 import graphQlService from "@/graphql/graphqlService";
 import { useCreateUserMutation } from "@/services/api/fileServer";
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
 
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  //   const signupMutation = useCreateUserMutation(graphQlService(),{
+  const signupMutation = useCreateUserMutation(graphQlService(), {
+    onSuccess() {
+      console.log("Successfully Signed Up");
+    },
+    onError(error: any) {
+      console.log(error);
+    },
+  });
 
-  //   })
-  const handleLogin = () => {
-    // Add your authentication logic here
-    console.log(`Logging in with username: ${name} and password: ${password}`);
+  const handleSubmit = (
+    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) => {
+    e.preventDefault();
+    console.log("Here", name, email, password);
+    signupMutation.mutate({ createUserInput: { name, email, password } });
   };
 
-  return <SignupForm></SignupForm>;
+  return (
+    <SignupForm
+      name={name}
+      email={email}
+      password={password}
+      setName={setName}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      onSubmit={handleSubmit}
+    ></SignupForm>
+  );
 };
 
 export default SignUp;
